fa2dc131ece35e4423bdecfd2f63fdd9a5fd75ec
Jonathan Vilaça <jfv2234@users.noreply.github.com>
2020-11-02 12:56:31 -0300

    commit

--- lib/models/buscalivros.dart
+++ lib/models/buscalivros.dart
@@ -16,34 +16,36 @@ class _buscalivrosState extends State<buscalivros>{
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
-    return Scaffold(
-      resizeToAvoidBottomPadding: false,
-      backgroundColor: Colors.blue,
-      body: Container(
-        width: size.width,
-        height: size.height,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-            image: AssetImage('images/login.png'),
-            fit: BoxFit.cover,
-            ),
-        ),
-        child: 
-          Column(
-            children: <Widget>[
-              SizedBox(
-                  height: size.height*.23,
-                  width: size.width,
-                  child:
-                    Container(alignment: Alignment.bottomLeft,
-                      child: Text("Pesquise um título de livro:", 
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16),
-                      textAlign: TextAlign.left),
-                    ), 
+    var buscador;
+        return Scaffold(
+          resizeToAvoidBottomPadding: false,
+          backgroundColor: Colors.blue,
+          body: Container(
+            width: size.width,
+            height: size.height,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                image: AssetImage('images/login.png'),
+                fit: BoxFit.cover,
                 ),
-                  TextField(
+            ),
+            child: 
+              Column(
+                children: <Widget>[
+                  SizedBox(
+                      height: size.height*.23,
+                      width: size.width,
+                      child:
+                        Container(alignment: Alignment.bottomLeft,
+                          child: Text("Pesquise um título de livro:", 
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16),
+                          textAlign: TextAlign.left),
+                        ), 
+                    ),
+                      TextFormField(
+                        controller: buscador,
                     onChanged: (String busca) => {
                       setState((){
                         pesquisa = busca;
@@ -79,16 +81,45 @@ class _buscalivrosState extends State<buscalivros>{
                             itemBuilder: (_, index){
                               return
                                 ListTile(
-                                  title: Text(snapshot.data[index].data['titulo']),
-                                  subtitle: Text('Código: '+snapshot.data[index].data['codigo'] + 
-                                  ' | Autor: '+snapshot.data[index].data['autor']
-                                  ),leading: Column(
+                                  title: 
+                                  ExpansionTile(
+                                    title: Text(snapshot.data[index].data['titulo']),
+                                    subtitle: Text('Cod: '+snapshot.data[index].data['codigo'] + 
+                                  ' | Autor: '+snapshot.data[index].data['autor'],),
+                                  children: [
+                                    Text('Editora: '+snapshot.data[index].data['editora'] + '\n'
+                                  'Gênero: '+snapshot.data[index].data['genero'] + '\n'
+                                  'Ano: '+snapshot.data[index].data['ano'],),
+                                  ],
+                                  ),leading: Column( 
                                     children: <Widget>[
                                       IconButton(
-                                        icon: const Icon(Icons.delete), onPressed: (){
-                                          deletarLivro(context, snapshot.data[index], index);
-                                          setState(() {
-                                            snapshot.data.removeAt(index);
+                                        icon: const Icon(Icons.delete), 
+                                        onPressed: (){
+                                          showCupertinoDialog(
+                                          context: context, 
+                                          builder: (context){
+                                            return CupertinoAlertDialog(
+                                              title: Text('ATENÇÃO!'),
+                                              content: Text('Deseja realmente deletar o livro?'),
+                                              actions: [
+                                                CupertinoDialogAction(child: Text('SIM'),
+                                                isDestructiveAction: true,
+                                                onPressed: (){
+                                                  deletarLivro(context, snapshot.data[index], index);
+                                                  setState(() {
+                                                    snapshot.data.removeAt(index);
+                                                  });
+                                                },
+                                                ),
+                                                CupertinoDialogAction(child: Text('NÃO'),
+                                                isDestructiveAction: true,
+                                                onPressed: (){
+                                                  Navigator.pop(context);
+                                                },
+                                                ),
+                                              ],
+                                            );
                                           });
                                         })
                                     ],
